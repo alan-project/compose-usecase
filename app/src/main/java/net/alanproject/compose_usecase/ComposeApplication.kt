9aa25cd13ca21fa4c058729e6a210598f086887b
alan-project <banana205@gmail.com>
2022-01-19 16:37:52 -0500

    applied Timber for logging

---
+++ app/src/main/java/net/alanproject/compose_usecase/ComposeApplication.kt
@@ -0,0 +1,12 @@
+package net.alanproject.compose_usecase
+
+import android.app.Application
+import timber.log.Timber
+
+class ComposeApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
